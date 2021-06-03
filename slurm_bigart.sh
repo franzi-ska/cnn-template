b451cff1a27bf38d2ffd5a5d3d9bf65ac062b5e5
franziska <franziska.h.knuth@ntnu.no>
2021-06-03 23:48:16 +0200

    Update config files for BiGART abstract

---
+++ slurm_bigart.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#SBATCH --ntasks=1               # 1 core(CPU)
+#SBATCH --nodes=1                # Use 1 node
+#SBATCH --job-name=bigart   # sensible name for the job
+#SBATCH --mem=8G                 # Default memory per CPU is 3GB.
+#SBATCH --partition=gpu # Use the verysmallmem-partition for jobs requiring < 10 GB RAM.
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=franziska.h.knuth@ntnu.no # Email me when job is done.
+#SBATCH --mail-type=ALL
+#SBATCH --output=outputs/training_%A.out
+#SBATCH --error=outputs/training_%A.out
+
+
+# If you would like to use more please adjust this.
+
+## Below you can put your scripts
+# If you want to load module
+module load singularity
+
+## Code
+# If data files aren't copied, do so
+#!/bin/bash
+if [ $# -lt 1 ];
+    then
+    printf "Not enough arguments - %d\n" $#
+    exit 0
+    fi
+
+if [ ! -d "$TMPDIR/$USER/hn_delin" ]
+    then
+    echo "Didn't find dataset folder. Copying files..."
+    mkdir --parents $TMPDIR/$USER/hn_delin
+    fi
+
+for f in $(ls $HOME/datasets/*)
+    do
+    FILENAME=`echo $f | awk -F/ '{print $NF}'`
+    echo $FILENAME
+    if [ ! -f "$TMPDIR/$USER/hn_delin/$FILENAME" ]
+        then
+        echo "copying $f"
+        cp -r $HOME/datasets/$FILENAME $TMPDIR/$USER/hn_delin/
+        fi
+    done
+
+
+echo "Finished seting up files."
+
+echo $1
+
+# Hack to ensure that the GPUs work
+nvidia-modprobe -u -c=0
+
+
+# Run experiment
+singularity exec --nv deoxys.sif python experiment.py config/$1.json $HOME/performance/$1 --epochs 200  --model_checkpoint_period 1 --prediction_checkpoint_period 1 ${@:2}
---
+++ write_config_from_csv.py
@@ -0,0 +1,58 @@
+import json
+import os
+
+import pandas as pd
+
+def write_file(h5_file, n_channel, test_fold, learning_rate, config_filename):
+    template_config = 'bigart_template.json'
+    with open(template_config) as f:
+        json_data_template = json.load(f)
+
+    fold_list = [1, 2, 3, 4, 5]
+    json_data_template["dataset_params"]["config"]["filename"] = h5_file
+    json_data_template["dataset_params"]["config"]["test_folds"] = [
+    str(test_fold) + "/352"]
+    json_data_template["dataset_params"]["config"]["val_folds"] = [
+    str(test_fold) + "/352"]
+    json_data_template["dataset_params"]["config"]["train_folds"] = [
+    str(i) + "/352" for i in fold_list if not i == test_fold]
+
+    json_data_template["input_params"]["shape"] = [352, 352, n_channel]
+    json_data_template["model_params"]['optimizer']['config']['learning_rate'] = learning_rate
+    # write file to disk
+    with open(config_filename, 'w') as json_file:
+        json.dump(json_data_template, json_file)
+
+
+def read_csv_and_write_config_files():
+    data = pd.read_csv('experiments.csv')
+
+
+    for (row_id, row) in data.iterrows():
+        print(row_id)
+        h5_file = '/net/fs-1/home01/franzikn/datasets/'
+        config_filename = os.path.join( 'config', row['ID']+'.json')
+        test_fold = row['validation_fold']
+        learning_rate = row['Learning_rate']
+
+        if row['Dataset'] == 'Oxytarget':
+            h5_file += 'OxyTarget_'
+        if row['Channels'] == 'T2w':
+            n_channel = 1
+            h5_file += 'T2_'
+        elif row['Channels'] == 'T2w + DW':
+            n_channel = 2
+            h5_file += 'T2DW_'
+        else:
+            raise Exception('Check for typos in Channels')
+
+        if row['Normalisation'] == 'Z-Score':
+            h5_file += 'ZScore.h5'
+        elif row['Normalisation'] == 'Z-Score + HM':
+            h5_file += 'MHZscore.h5'
+        else:
+            raise Exception('Check for typos in Normalisation')
+
+        write_file(h5_file, n_channel, test_fold, learning_rate, config_filename)
+
+read_csv_and_write_config_files()

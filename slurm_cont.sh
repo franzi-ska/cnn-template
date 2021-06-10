896b9c9f6b58f9880f18e44816164eb91538907a
franziska <franziska.h.knuth@ntnu.no>
2021-06-10 12:59:50 +0200

    uptadte slurm_cont

--- slurm_cont.sh
+++ slurm_cont.sh
@@ -5,8 +5,6 @@
 #SBATCH --mem=16G                 # Default memory per CPU is 3GB.
 #SBATCH --partition=gpu # Use the verysmallmem-partition for jobs requiring < 10 GB RAM.
 #SBATCH --gres=gpu:1
-#SBATCH --mail-user=ngochuyn@nmbu.no # Email me when job is done.
-#SBATCH --mail-type=ALL
 #SBATCH --output=outputs/unet-%A.out
 #SBATCH --error=outputs/unet-%A.out
 
@@ -31,14 +29,14 @@ if [ ! -d "$TMPDIR/$USER/hn_delin" ]
     mkdir --parents $TMPDIR/$USER/hn_delin
     fi
 
-for f in $(ls $HOME/datasets/headneck/*)
+for f in $(ls $HOME/datasets/*)
     do
     FILENAME=`echo $f | awk -F/ '{print $NF}'`
     echo $FILENAME
     if [ ! -f "$TMPDIR/$USER/hn_delin/$FILENAME" ]
         then
         echo "copying $f"
-        cp -r $HOME/datasets/headneck/$FILENAME $TMPDIR/$USER/hn_delin/
+        cp -r $HOME/datasets/$FILENAME $TMPDIR/$USER/hn_delin/
         fi
     done
 
@@ -49,4 +47,4 @@ echo "Finished seting up files."
 nvidia-modprobe -u -c=0
 
 # Run experiment
-singularity exec --nv deoxys.sif python continue_experiment.py $1 $HOME/hnperf/$2 --epochs $3 ${@:4}
+singularity exec --nv deoxys.sif python continue_experiment.py $1 $HOME/performance/$2 --epochs $3 ${@:4}
